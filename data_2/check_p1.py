c58ba3bdfd0dba0eedd9181fd76bac9f0edd5624
Harshith <harshithb.ecs24@chanakyauniversity.edu.in>
2026-09-18 16:17:04 +0530

    Delete data_2 directory

--- data_2/check_p1.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-con = duckdb.connect()
-df = con.execute("SELECT notice_id, portal_id, body FROM 'notices/*.csv' WHERE portal_id IN ('P001', 'P002') LIMIT 5").df()
-
-for i, row in df.iterrows():
-    print(f"--- Notice {row['notice_id']} ({row['portal_id']}) ---")
-    print(row['body'][:600])
